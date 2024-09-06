3cf4b65a72c60b143c39f7c4d98f8b5f2b56c101
vexsmart <programador.vitor.ribeiro@hotmail.com>
2024-09-06 17:15:35 -0300

    final commit

---
+++ phonebook backend/index.js
@@ -0,0 +1,102 @@
+var express = require("express");
+var morgan = require("morgan");
+var cors = require("cors");
+
+const app = express();
+
+
+app.use(morgan("tiny"));
+
+app.use(cors());
+app.use(express.json());
+const PORT = 3001;
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.put("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (!person) {
+    return response.status(404).end();
+  }
+  const body = request.body;
+  person.name = body.name;
+  person.number = body.number;
+  response.json(person);
+})
+
+app.post("/api/persons", (request, response) => {
+  const id = Math.floor(Math.random() * 1000 + 1).toString();
+  const body = request.body;
+
+  const newPerson = { id: id, name: body.name, number: body.number };
+
+  const sameName = persons.find((person) => person.name === body.name);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name missing or number missing",
+    });
+  } else if (sameName) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  persons.push(newPerson);
+  response.json(newPerson);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
